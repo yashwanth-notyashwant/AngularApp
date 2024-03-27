e1048b8b2b234c721be3843dbb62a2de787c637a
yashwanth <yashwanth051202@gmail.com>
2024-03-27 16:09:26 +0530

    Angular app that has functions like prop drilling only single instance

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'testApp';
+// }
+
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img 
+        src="https://r.bing.com/rp/bh44AmEP6-vpc413zKIP0fMCrCU.png" alt="logo"
+        aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Title';
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+
+      
+    </section>
+  `,
+
+  styleUrl: './home.component.css',
+})
+  
+  // here we have done property binding in this line  <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+export class HomeComponent {
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `https://r.bing.com/rp/bh44AmEP6-vpc413zKIP0fMCrCU.png`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
